b6677dbceb8ecae0b04f64302550e0fa5ddd999c
Ghost1227 <dgriffiths@section214.com>
2014-12-01 02:52:47 -0600

    Rework JS

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,7 @@
+/*global jQuery, document*/
 jQuery(document).ready(function($) {
+
+    // Setup datepickers
 	if($('.edd_pl_datepicker').length ) {
 		var dateFormat = 'mm/dd/yy';
 		$('.edd_pl_datepicker').datetimepicker({
@@ -8,21 +11,64 @@ jQuery(document).ready(function($) {
 			showButtonPanel: true
 		});
 	}
-    if($('#edd_settings\\[edd_purchase_limit_g_start_date\\]').length ) {
-		$('#edd_settings\\[edd_purchase_limit_g_start_date\\]').datetimepicker({
+
+    if($("input[name='edd_settings[edd_purchase_limit_g_start_date]']").length ) {
+		$("input[name='edd_settings[edd_purchase_limit_g_start_date]']").datetimepicker({
 			dateFormat: dateFormat,
 			showOtherMonths: true,
 			selectOtherMonths: true,
 			showButtonPanel: true
 		});
-		$('#edd_settings\\[edd_purchase_limit_g_end_date\\]').datetimepicker({
+		$("input[name='edd_settings[edd_purchase_limit_g_end_date]']").datetimepicker({
 			dateFormat: dateFormat,
 			showOtherMonths: true,
 			selectOtherMonths: true,
 			showButtonPanel: true
 		});
     }
+
     $('.edd_pl_datepicker').clearable();
-	$('#edd_settings\\[edd_purchase_limit_g_start_date\\]').clearable();
-	$('#edd_settings\\[edd_purchase_limit_g_end_date\\]').clearable();
+	$("input[name='edd_settings[edd_purchase_limit_g_start_date]']").clearable();
+	$("input[name='edd_settings[edd_purchase_limit_g_end_date]']").clearable();
+
+    // Show remaining settings
+    $("input[name='edd_settings[edd_purchase_limit_show_counts]']").change(function () {
+        var selectedItem = $("input[name='edd_settings[edd_purchase_limit_show_counts]']").is(':checked');
+
+        if (selectedItem === true) {
+            $("input[name='edd_settings[edd_purchase_limit_remaining_label]']").closest('tr').css('display', 'table-row');
+        } else {
+            $("input[name='edd_settings[edd_purchase_limit_remaining_label]']").closest('tr').css('display', 'none');
+        }
+    }).change();
+
+    // Restrict date settings
+    $("input[name='edd_settings[edd_purchase_limit_restrict_date]']").change(function () {
+        var selectedItem = $("input[name='edd_settings[edd_purchase_limit_restrict_date]']").is(':checked');
+        
+        if (selectedItem === true) {
+            $("input[name='edd_settings[edd_purchase_limit_g_start_date]']").closest('tr').css('display', 'table-row');
+            $("input[name='edd_settings[edd_purchase_limit_g_end_date]']").closest('tr').css('display', 'table-row');
+            $("input[name='edd_settings[edd_purchase_limit_pre_date_label]']").closest('tr').css('display', 'table-row');
+            $("input[name='edd_settings[edd_purchase_limit_post_date_label]']").closest('tr').css('display', 'table-row');
+        } else {
+            $("input[name='edd_settings[edd_purchase_limit_g_start_date]']").closest('tr').css('display', 'none');
+            $("input[name='edd_settings[edd_purchase_limit_g_end_date]']").closest('tr').css('display', 'none');
+            $("input[name='edd_settings[edd_purchase_limit_pre_date_label]']").closest('tr').css('display', 'none');
+            $("input[name='edd_settings[edd_purchase_limit_post_date_label]']").closest('tr').css('display', 'none');
+        }
+    }).change();
+
+    // Error handler settings
+    $("select[name='edd_settings[edd_purchase_limit_error_handler]']").change(function () {
+        var selectedItem = $("select[name='edd_settings[edd_purchase_limit_error_handler]'] option:selected");
+
+        if (selectedItem.val() === 'redirect') {
+            $("input[name='edd_settings[edd_purchase_limit_error_message]']").closest('tr').css('display', 'none');
+            $("select[name='edd_settings[edd_purchase_limit_redirect_url]']").closest('tr').css('display', 'table-row');
+        } else {
+            $("input[name='edd_settings[edd_purchase_limit_error_message]']").closest('tr').css('display', 'table-row');
+            $("select[name='edd_settings[edd_purchase_limit_redirect_url]']").closest('tr').css('display', 'none');
+        }
+    }).change();
 });
